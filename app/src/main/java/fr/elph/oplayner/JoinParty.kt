ace9d064364a2fb1259be9aec82983ea6aed2bd6
eltha9 <theadosto@gmail.com>
2019-05-19 16:14:16 +0200

    add launcher icon

--- app/src/main/java/fr/elph/oplayner/JoinParty.kt
+++ app/src/main/java/fr/elph/oplayner/JoinParty.kt
@@ -6,10 +6,18 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import android.content.Intent
+import android.content.Context.TELEPHONY_SERVICE
+import androidx.core.content.ContextCompat.getSystemService
+import android.telephony.TelephonyManager
+import android.content.Context
+
 
 class JoinParty : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.join_party)
     }
+
+    //var tMgr = mAppContext.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+    //var mPhoneNumber = tMgr.line1Number
 }
