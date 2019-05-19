34fc9030a372b26e98403a6f70b60e4461b0126c
eltha9 <theadosto@gmail.com>
2019-05-19 12:39:51 +0200

    add join and create activity, remove top bar

---
+++ app/src/main/java/fr/elph/oplayner/JoinParty.kt
@@ -0,0 +1,15 @@
+package fr.elph.oplayner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import android.content.Intent
+
+class JoinParty : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.join_party)
+    }
+}
--- app/src/main/java/fr/elph/oplayner/MainActivity.kt
+++ app/src/main/java/fr/elph/oplayner/MainActivity.kt
@@ -2,12 +2,39 @@ package fr.elph.oplayner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import android.content.Intent
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+    private lateinit var buttonJoin:Button
+    private lateinit var buttonCreate:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonJoin = findViewById(R.id.join_button)
+        buttonCreate = findViewById(R.id.create_button)
+
+        buttonJoin.setOnClickListener { view ->
+
+            val intent = Intent(this, JoinParty::class.java)
+            startActivity(intent)
+        }
+        buttonCreate.setOnClickListener { view ->
+
+            val intent = Intent(this, CreateParty::class.java)
+            startActivity(intent)
+        }
+
     }
 
 
