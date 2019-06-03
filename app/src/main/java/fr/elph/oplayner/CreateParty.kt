166121019fe8c05622172b817f9f5599d2d476f3
eltha9 <theadosto@gmail.com>
2019-06-03 18:04:24 +0200

    prevent keyboard to show at start activity, add placeholder

--- app/src/main/java/fr/elph/oplayner/CreateParty.kt
+++ app/src/main/java/fr/elph/oplayner/CreateParty.kt
@@ -20,7 +20,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 */
 
 class CreateParty : AppCompatActivity(){
-
+    
     private lateinit var buttonCreate:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
